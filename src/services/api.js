f022555feb4b71bb62191a92ba04fa5d1f7da6b0
Gabriel Almeida de Oliveira <eng.gabriel.ao@gmail.com>
2022-04-26 18:08:58 -0300

    Requisito 1 concluido - Comentarios adicionados a api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,16 @@
 export async function getCategories() {
-  const endpoint = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/categories'; // Endereco para requisicao de categoras da API
 
-  const categories = await fetch(endpoint)
+  const categories = await fetch(endpoint) // O fetch obtem a resposta do endpoint selecionado e depois essa reposta e convertida em um objeto json que pode ser trabalhado pela aplicacao
     .then((response) => response.json());
 
   return categories;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`; // Endereco de pesquisa por categoria e palavra-chave na API
 
-  const products = await fetch(endpoint)
+  const products = await fetch(endpoint) // O fetch obtem a resposta do endpoint selecionado e depois essa reposta e convertida em um objeto json que pode ser trabalhado pela aplicacao
     .then((response) => response.json());
 
   return products;
