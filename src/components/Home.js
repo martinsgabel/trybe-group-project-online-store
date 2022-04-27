1c09b54a7df7f27129a589251de24c8d3167d385
Júlia <juuliafariias@gmail.com>
2022-04-27 19:01:55 -0300

    Requisito 3 - completo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import ShoppingCartButton from './ShoppingCartButton';
 
 class Home extends React.Component {
   render() {
     return (
-      <section className="main-section-home">
-        <input type="text" />
-        <button type="button">Lupa</button>
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
-      </section>
+      <div>
+        <section className="main-section-home">
+          <input type="text" />
+          <button type="button">Lupa</button>
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        </section>
+        <ShoppingCartButton />
+      </div>
     );
   }
 }
---
+++ src/components/ShoppingCartButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class ShoppingCartButton extends React.Component {
+  render() {
+    return(
+      <div>
+        <Link to="/cart" data-testid="shopping-cart-button">
+        <img
+            className="cart-image"
+            width="40" height="40"
+            src="https://cdn-icons-png.flaticon.com/512/263/263142.png"
+            alt="imagem-do-carrinho-de-compras"
+          />
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default ShoppingCartButton;
